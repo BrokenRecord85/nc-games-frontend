1c70131ff4be52c24381a13fd2984aa0805c8c71
BrokenRecord85 <pawel_kunicki@hotmail.com>
2022-09-28 12:25:52 +0100

    Finish ticket 7

---
+++ src/components/Votes.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react'
+import {useState} from 'react'
+import { castVote } from '../utils/api'
+import {AiOutlineLike, AiTwotoneLike} from 'react-icons/ai'
+
+const Votes = ({votes, review_id}) => {
+  
+  const [currVotes, setCurrVotes] = useState(votes)
+  const [hasVoted, setHasVoted] = useState(false)
+  useEffect(() => {
+        setCurrVotes(votes)
+  }, [votes])
+  
+
+  const handleVote = () => {
+    if(!hasVoted) {
+        setCurrVotes(currVotes + 1)
+        setHasVoted(true)
+      const reqBody = {
+          inc_votes: 1
+      }
+      castVote(review_id, reqBody)
+      .then(({data}) => {
+          return data
+      })
+    }
+    else {
+      setCurrVotes(currVotes - 1)
+      setHasVoted(false)
+    const reqBody = {
+        inc_votes: 1
+    }
+    castVote(review_id, reqBody)
+    .then(({data}) => {
+        return data
+    })
+    }
+    
+  }
+
+  return (
+    <div className='voting-container'>
+      {!hasVoted ? <AiOutlineLike size={32} onClick={handleVote}/> : <AiTwotoneLike size={32} onClick={handleVote}/>}  
+    <p>Votes: {currVotes}</p>    
+    </div>
+  )
+}
+
+export default Votes
